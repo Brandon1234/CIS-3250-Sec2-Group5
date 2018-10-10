c40e225795aa3efd13658509f03e0483107e6e5f
KimKrispy <43280147+KimKrispy@users.noreply.github.com>
2018-10-10 15:21:26 -0400

    refactored factorial
    Still not fully sure as to the purpose of this header file, requires discussion if possible

--- src/factorial.h
+++ src/factorial.h
@@ -2,16 +2,21 @@
 #include <stdint.h>
 
     unsigned long long int fact(int n){/*Factorial function inefficient for values after 20*/
+        
         //variable declarations
         unsigned long long int fact;
         int i;
+        
+        //initialising variables
         fact=1;
         
         for(i=n;i>0;i--){
+            
             fact*=i;
+        
         }
         
         return fact;
         
-    } //unsigned long long int fact(int n)
+    } //end of unsigned long long int fact(int n);
 

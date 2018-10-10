f3740aa070319dfdbac51b4153a3f81b70c0eba8
KimKrispy <43280147+KimKrispy@users.noreply.github.com>
2018-10-10 15:16:14 -0400

    Update factorial.h

--- src/factorial.h
+++ src/factorial.h
@@ -2,13 +2,16 @@
 #include <stdint.h>
 
     unsigned long long int fact(int n){/*Factorial function inefficient for values after 20*/
-    unsigned long long int fact;
-    int i;
-    fact=1;
-
-    for(i=n;i>0;i--){
-        fact*=i;
-    }
-    return fact;
-    }
+        //variable declarations
+        unsigned long long int fact;
+        int i;
+        fact=1;
+        
+        for(i=n;i>0;i--){
+            fact*=i;
+        }
+        
+        return fact;
+        
+    } //unsigned long long int fact(int n)
 

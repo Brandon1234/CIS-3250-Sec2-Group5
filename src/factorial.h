73f7c598744f6dc647cb26dfd6676be2a82da8ca
KimKrispy <43280147+KimKrispy@users.noreply.github.com>
2018-11-02 13:57:20 -0400

    oops, forgot to commit changes to factorial.h, fixed that and also second half of conversions.h

--- src/factorial.h
+++ src/factorial.h
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
+/*changelog
 
-    unsigned long long int fact(int n){/*Factorial function inefficient for values after 20*/
-        
-        //variable declarations
-        unsigned long long int fact;
-        int i;
-        
-        //initialising variables
-        fact=1;
-        
-        for(i=n;i>0;i--){
-            
-            fact*=i;
-        
-        }
-        
-        return fact;
-        
-    } //end of unsigned long long int fact(int n);
+cleaned up a bit, adding spaces and to make things easier to read
+added comments to explain things
+that's about it really...
+*///Last Edited by Alexander Kim 31/10/2018 7:49PM
+
+unsigned long long int fact(int n) {/*Factorial function inefficient for values after 20*/
+	
+	//variable declaration
+	unsigned long long int fact;
+	int i;
+
+	fact=1;
+
+	//factorials works simply as: fact(3) = 1 * 2 * 3 or fact(8) = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+	for( i = n; i > 0; i-- ){
+
+		//same as doing fact = fact * i
+		fact *= i;
+
+	}
+		
+	return fact;
+
+}//unsigned long long int fact
 

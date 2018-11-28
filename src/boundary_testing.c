5ca379c49d14957b5d8c7282e74c87c992d0a69f
David Reti <dreti@uoguelph.ca>
2018-11-28 14:10:36 -0500

    Added file for boundary testing

---
+++ src/boundary_testing.c
@@ -0,0 +1,17 @@
+
+/*This function boundary tests the last 4 loops in unit_test.c and the else statement at the end of the file
+ *Returns the number of errors that occured during testing and also prints the results of each test
+ *Written by David Reti on 2018/11/28
+ */
+unsigned int boundaryTestLoop4
+{
+	
+	unsigned int numErrors = 0;
+
+	printf("Boundary testing last 4 loops\n\n");
+
+	printf("Finished boundary testing last 4 loops\n\n");
+
+	return numErrors;
+
+}

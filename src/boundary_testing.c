0672397d5c36e7f051e55fb2be897606dd5ac49b
David Reti <dreti@uoguelph.ca>
2018-11-29 17:29:33 -0500

    Added an additional funtion for testing loop bounds and wrote the lower boundary tests for the last 4 loops

--- src/boundary_testing.c
+++ src/boundary_testing.c
@@ -1,26 +1,66 @@
 #include "unit_testing.c"
 
-
 /*This function boundary tests the last 4 loops in unit_test.c and the else statement at the end of the file
  *Returns the number of errors that occured during testing and also prints the results of each test
  *Written by David Reti on 2018/11/28
  */
-unsigned int boundaryTestLoop4
+unsigned int boundaryTestLoop4()
 {
 	
 	unsigned int numErrors = 0;
+	unsigned int* testResults;
 
 	printf( "Boundary testing last 4 loops and else statement\n\n" );
 
-	printf( "Testing upper limits:\n" );
+	printf( "Testing upper limits:\n\n" );	
 
-	matrix_sum(3,3,3,3);	
+	printf( "Testing lower limits:\n\n" );
 
-	printf( "Testing lower limits:\n" );
+	//Test setting loops to lower bounds, run with matrices all of size 0
+	printf( "Test 1: running function with matrices all of size 0\nExpected: all loops run 0 times\n" );
+	testResults = last4loops( 0, 0, 0, 0 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3]);
 
-	printf( "Testing middle values:\n" );
+	if(testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0)
+	{
+		printf("Test Passed\n");
+	}
+	else
+	{
+		printf("Test Failed\n");
+		numErrors++;
+	}
 
+	free(testResults);
+
+	//Test setting loops out of lower bounds, run with matrices all of size -1
+	printf( "Test 1: running function with matrices all of size -1\nExpected: all loops run 0 times\n" );
+	testResults = last4loops( -1, -1, -1, -1 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3]);
+
+	if(testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0)
+	{
+		printf("Test Passed\n");
+	}
+	else
+	{
+		printf("Test Failed\n");
+		numErrors++;
+	}
+
+	free(testResults);
+
+	printf( "Testing middle values:\n\n" );
+
+	/*
 	printf( "Testing else statememt:\n" );
+	
+	testResults = last4loops( 1, 1, 1, 1 );
+	free(testResults);
+
+	testResults = last4loops( 1, 1, 2, 2 );
+	free(testResults);
+	*/
 
 	printf( "Finished boundary testing last 4 loops and else statement\n\n" );
 
--- src/unit_testing.h
+++ src/unit_testing.h
@@ -56,3 +56,61 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
     }
 
 }
+
+//This function tests the last 4 loops in the matrix sum function as well as the else statement at the end
+//Instead of asking the user for input and printing the results to the screen, it uses identity matrices
+//Returns the number of times each loop ran, as well as weather the else statement was hit (0 or 1)
+unsigned int* last4loops( int matrix1Rows, int matrix1Cols, int matrix2Rows, int matrix2Cols )
+{
+	unsigned int* toReturn = malloc( sizeof( unsigned int ) * 5 );
+	memset( (void*)toReturn, 0, 5 );
+
+	//if both matrices have the same number of rows and columns
+	if( matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols ) {
+
+		float a[matrix1Rows][matrix1Cols];
+		float b[matrix2Rows][matrix2Cols];
+		float c[matrix1Rows][matrix1Cols];
+
+		//Set the matrices to be zero
+		memset( (void*)a, 0, matrix1Rows * matrix1Cols );
+		memset( (void*)b, 0, matrix2Rows * matrix2Cols );
+		memset( (void*)c, 0, matrix1Rows * matrix1Cols );
+		
+		//Run the loops and count the number of times each loop ran
+		//using matrix addition, adds each row and column value together to get a new matrix
+		for ( i = 0; i < matrix1Rows; i++ ) {
+
+			for ( j = 0; j < matrix1Cols; j++ ) {
+				
+				c[i][j]=a[i][j]+b[i][j];
+				toReturn[1]++;
+
+			}
+
+			toReturn[0]++;
+
+		}
+		
+		//outputs the new matrix as a table
+        	for ( j = 0; j < matrix1Cols; j++ ) {
+
+			for (i = 0; i < matrix1Rows; i++) {
+				
+       				toReturn[3]++;
+				
+			}
+			
+			toReturn[2]++;
+
+		}
+
+	
+	} else {
+		
+		toReturn[4] = 1;
+
+	}
+
+	return toReturn;
+}

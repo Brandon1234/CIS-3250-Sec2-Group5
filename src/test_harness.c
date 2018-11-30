5bf31ea5654006d6bd8939c82fedebb03356d7e1
Brandon1234 <brandonlit@gmail.com>
2018-11-30 16:18:23 -0500

    Incorperated boundary testing

--- src/test_harness.c
+++ src/test_harness.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "unit_testing.h"
+#include "unit_testing.c"
 
 //Abdalaziz Elshamli tesing
 
@@ -451,7 +453,166 @@ int forLoopThreeTestFour()
     }
 }
 
+/*This function boundary tests the last 4 loops in unit_test.c and the else statement at the end of the file
+ *Returns the number of errors that occured during testing and also prints the results of each test
+ *Written by David Reti on 2018/11/28
+ */
+unsigned int boundaryTestLoop4()
+{
+	
+	unsigned int numErrors = 0;
+	unsigned int* testResults;
+
+	printf( "Boundary testing last 4 loops\n\n" );
+
+	printf( "Testing upper limits:\n\n" );	
+
+	//Test setting loops to lower bounds, run with matrices all of size 0
+	printf( "Test 1: running function with matrices all of size 512\nExpected: all loops run 0 times\n" );
+	testResults = last4loops( 512, 512, 512, 512 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3] );
+
+	if( testResults[0] == 512 && testResults[1] == 262144 && testResults[2] == 512 && testResults[3] == 262144 ) {
+		printf( "Test Passed\n" );
+	}
+	else {
+		printf( "Test Failed\n" );
+		numErrors++;
+	}
+
+	printf( "Testing lower limits:\n\n" );
+
+	//Test setting loops to lower bounds, run with matrices all of size 0
+	printf( "Test 2: running function with matrices all of size 0\nExpected: all loops run 0 times\n" );
+	testResults = last4loops( 0, 0, 0, 0 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n\n", testResults[0], testResults[1], testResults[2], testResults[3] );
+
+	if( testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0 ) {
+		printf( "Test Passed\n" );
+	}
+	else {
+		printf( "Test Failed\n" );
+		numErrors++;
+	}
+
+	//Test setting loops out of lower bounds, run with matrices all of size -1
+	printf( "Test 3: running function with matrices all of size -1\nExpected: all loops run 0 times\n" );
+	testResults = last4loops( -1, -1, -1, -1 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n\n", testResults[0], testResults[1], testResults[2], testResults[3] );
+
+	if( testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0 ) {
+		printf( "Test Passed\n" );
+	}
+	else {
+		printf( "Test Failed\n" );
+		numErrors++;
+	}
+
+	printf( "Testing middle values:\n\n" );
+
+	//Test middle / average loop values, just to make sure the loops actually run correctly
+	printf( "Test 4: running function with middle values, matrices size 3 x 3\nExpected: loops 1 & 3 run 3 times\n          loops 2 & 4 run 9 times\n" );
+	testResults = last4loops( 3, 3, 3, 3 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n\n", testResults[0], testResults[1], testResults[2], testResults[3] );
+
+	if( testResults[0] == 3 && testResults[1] == 9 && testResults[2] == 3 && testResults[3] == 9 ) {
+		printf( "Test Passed\n" );
+	}
+	else {
+		printf( "Test Failed\n" );
+		numErrors++;
+	}
+
+	free( testResults );
+
+	printf( "Finished boundary testing last 4 loops\n\n" );
+	printf( "Total errors: %u\n", numErrors );
+
+	return numErrors;
+
+}
+
+
+/*This function boundary tests the first 4 loops in unit_test.c and the initial if statement.
+ *Returns the number of errors that occured during testing and also prints the results of each test
+ *Written by Luca Bozzetto on 2018/11/29
+ */
+unsigned int boundaryTestFirstLoops()
+{
+    unsigned int numErrors = 0;
+    
+    printf( "Boundary testing first 4 loops and initial if statement\n\n" );
+    
+    printf( "1. Testing matrixes with 0 columns and 0 rows:\n" );
+    if( zeroDimensionMatrix() ) {
+        
+        printf( "[TEST FAILED] Matrix with 0 rows and columns\n" );
+        numErrors++;
+        
+    }else {
+        
+        printf( "[TEST PASSED] Matrix with 0 rows and columns\n" );
+        
+    }
+    
+    printf( "2. Testing matrixes with different column values:\n" );
+    if( differentColumnsSize() ) {
+        
+        printf( "[TEST FAILED] First matrix with 2 columns and second matrix with 3 columns\n" );
+        numErrors++;
+        
+    }else {
+        
+        printf( "[TEST PASSED] First matrix with 2 columns and second matrix with 3 columns\n" );
+        
+    }
+    
+    printf( "3. Testing matrixes with different row values:\n" );
+    if( differentRowsSize() ) {
+        
+        printf( "[TEST FAILED] First matrix with 2 rows and second matrix with 3 rows\n" );
+        numErrors++;
+        
+    }else {
+        
+        printf( "[TEST PASSED] First matrix with 2 rows and second matrix with 3 rows\n" );
+        
+    }
+    
+    printf( "4. Testing matrixes with the a negative values for rows and columns:\n" );
+    if( negativeValues() ) {
+        
+        printf( "[TEST FAILED] Matrixes with negative values\n" );
+        numErrors++;
+        
+    }else {
+        
+        printf( "[TEST PASSED] Matrixes with negative values\n" );
+        
+    }
+    
+    printf( "5. Testing matrixes with the same number of rows and columns:\n" );
+    if( normalBehaviour() ) {
+        
+        printf( "[TEST FAILED] Matrixes with 2 rows and 2 columns\n" );
+        numErrors++;
+        
+    }else {
+        
+        printf( "[TEST PASSED] Matrixes with 2 rows and 2 columns\n" );
+        
+    }
+    
+    printf( "Completed testing first 4 loops and initial if statement with %d errors\n\n", numErrors);
+    
+    return numErrors;
+    
+}
+
+
 void structureTest(){
+	int result = 0;
+	int structurePassed = 0;
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	printf("Test 3: Structure \n");
 	printf("Test 3.1: Matrix Declaration\n");
@@ -770,8 +931,8 @@ int main(){
 	
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	printf("Test 1: Boundaries\n");
-	//boundTest = boundaryTestLoop4();
-	
+	boundaryTestLoop4();
+	boundaryTestFirstLoops();
 	if(boundTest > 0){
 		printf("Failed: %d\nSucceeded %d", boundTest, (4 - boundTest));
 	} else {
@@ -783,6 +944,6 @@ int main(){
 	printf("Test 2.1: \n");
 	printf("Test 2.2: \n");
 	
-	structureTest();
+	//structureTest();
 
 }
--- src/unit_testing.h
+++ src/unit_testing.h
@@ -1,4 +1,10 @@
-void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
+#include<string.h>
+
+float a[512][512];
+float b[512][512];
+float c[512][512];
+
+void matrix_sum(int matrix1Rows,int matrix1Cols,int matrix2Rows,int matrix2Cols){
 
 	//if both matrices have the same number of rows and columns
 	if(matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols){
@@ -8,9 +14,9 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
 		float c[matrix1Rows][matrix1Cols];
 
 		//gets the values for each row of matrix 1
-		for (i = 0; i < matrix1Rows; i++){
+		for (int i = 0; i < matrix1Rows; i++){
 			printf("Enter the members of matrix 1 row %d :",i+1);
-			for (j = 0; j < matrix1Cols; j++){
+			for (int j = 0; j < matrix1Cols; j++){
 				
 				scanf("%f",&a[i][j]);
 				
@@ -18,11 +24,11 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
 		}
 		
 		//gets the values for each row of matrix 2 
-		for (i = 0; i < matrix2Rows; i++){
+		for (int i = 0; i < matrix2Rows; i++){
 			
 			printf("Enter the members of matrix 2 row %d :",i+1);
 			
-			for (j = 0; j < matrix2Cols; j++){
+			for (int j = 0; j < matrix2Cols; j++){
 				
 				scanf("%f",&b[i][j]);
 				
@@ -30,8 +36,8 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
 		}
 		
 		//using matrix addition, adds each row and column value together to get a new matrix
-		for (i = 0; i < matrix1Rows; i++){
-			for (j = 0; j < matrix1Cols; j++){
+		for (int i = 0; i < matrix1Rows; i++){
+			for (int j = 0; j < matrix1Cols; j++){
 				
 				c[i][j]=a[i][j]+b[i][j];
 				
@@ -40,8 +46,8 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
 		
 		//outputs the new matrix as a table
 		printf("The sum of both matrices is\n:");
-        for (j = 0;j < matrix1Cols; j++){
-            for (i = 0; i < matrix1Rows; i++){
+        for (int j = 0;j < matrix1Cols; j++){
+            for (int i = 0; i < matrix1Rows; i++){
 				
                 printf("\t\t %.0f",c[i][j]);
 				
@@ -55,4 +61,58 @@ void matrix_sum(matrix1Rows,matrix1Cols,matrix2Rows,matrix2Cols){
 		
     }
 
+}
+
+//This function tests the last 4 loops in the matrix sum function as well as the else statement at the end
+//Instead of asking the user for input and printing the results to the screen, it uses identity matrices
+//Returns the number of times each loop ran, as well as weather the else statement was hit (0 or 1)
+unsigned int* last4loops( int matrix1Rows, int matrix1Cols, int matrix2Rows, int matrix2Cols )
+{
+	unsigned int* toReturn = (unsigned int *)malloc( sizeof( unsigned int ) * 5 );
+	memset( (void*)toReturn, 0, 5 * sizeof( unsigned int ));
+
+	//if both matrices have the same number of rows and columns
+	if( matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols ) {
+
+		//Set the matrices to be zero
+		memset( (void*)a, 0, matrix1Rows * matrix1Cols * sizeof(float) );
+		memset( (void*)b, 0, matrix2Rows * matrix2Cols * sizeof(float) );
+		memset( (void*)c, 0, matrix1Rows * matrix1Cols * sizeof(float) );
+		
+		//Run the loops and count the number of times each loop ran
+		//using matrix addition, adds each row and column value together to get a new matrix
+		for ( int i = 0; i < matrix1Rows; i++ ) {
+
+			for ( int j = 0; j < matrix1Cols; j++ ) {
+				
+				c[i][j]=a[i][j]+b[i][j];
+				toReturn[1]++;
+
+			}
+
+			toReturn[0]++;
+
+		}
+		
+		//outputs the new matrix as a table
+        	for ( int j = 0; j < matrix1Cols; j++ ) {
+
+				for ( int i = 0; i < matrix1Rows; i++) {
+				
+       				toReturn[3]++;
+				
+				}
+			
+			toReturn[2]++;
+
+		}
+
+	
+	} else {
+		
+		toReturn[4] = 1;
+
+	}
+
+	return toReturn;
 }

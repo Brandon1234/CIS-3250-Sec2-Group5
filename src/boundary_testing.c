a23dc9e5297283f7cfeb73d90047b6699716ced6
David Reti <dreti@uoguelph.ca>
2018-11-29 20:33:27 -0500

    Added middle value boundary testing

--- src/boundary_testing.c
+++ src/boundary_testing.c
@@ -10,7 +10,7 @@ unsigned int boundaryTestLoop4()
 	unsigned int numErrors = 0;
 	unsigned int* testResults;
 
-	printf( "Boundary testing last 4 loops and else statement\n\n" );
+	printf( "Boundary testing last 4 loops\n\n" );
 
 	printf( "Testing upper limits:\n\n" );	
 
@@ -19,38 +19,55 @@ unsigned int boundaryTestLoop4()
 	//Test setting loops to lower bounds, run with matrices all of size 0
 	printf( "Test 1: running function with matrices all of size 0\nExpected: all loops run 0 times\n" );
 	testResults = last4loops( 0, 0, 0, 0 );
-	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3]);
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3] );
 
-	if(testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0)
+	if( testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0 )
 	{
-		printf("Test Passed\n");
+		printf( "Test Passed\n" );
 	}
 	else
 	{
-		printf("Test Failed\n");
+		printf( "Test Failed\n" );
 		numErrors++;
 	}
 
-	free(testResults);
+	free( testResults );
 
 	//Test setting loops out of lower bounds, run with matrices all of size -1
 	printf( "Test 1: running function with matrices all of size -1\nExpected: all loops run 0 times\n" );
 	testResults = last4loops( -1, -1, -1, -1 );
-	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3]);
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3] );
 
-	if(testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0)
+	if( testResults[0] == 0 && testResults[1] == 0 && testResults[2] == 0 && testResults[3] == 0 )
 	{
-		printf("Test Passed\n");
+		printf( "Test Passed\n" );
 	}
 	else
 	{
-		printf("Test Failed\n");
+		printf( "Test Failed\n" );
 		numErrors++;
 	}
 
-	free(testResults);
+	free( testResults );
 
 	printf( "Testing middle values:\n\n" );
+
+	//Test middle / average loop values, just to make sure the loops actually run correctly
+	printf( "Test 1: running function with middle values, matrices size 3 x 3\nExpected: loops 1 & 3 run 3 times\n           loops 2 & 4 run 9 times\n" );
+	testResults = last4loops( -1, -1, -1, -1 );
+	printf( "Result: loop 1 ran %d times, loop 2 ran %d times, loop 3 ran %d times, loop 4 ran %d times\n", testResults[0], testResults[1], testResults[2], testResults[3] );
+
+	if( testResults[0] == 3 && testResults[1] == 9 && testResults[2] == 3 && testResults[3] == 9 )
+	{
+		printf( "Test Passed\n" );
+	}
+	else
+	{
+		printf( "Test Failed\n" );
+		numErrors++;
+	}
+
+	free( testResults );
 
 	/*
 	printf( "Testing else statememt:\n" );
@@ -62,7 +79,7 @@ unsigned int boundaryTestLoop4()
 	free(testResults);
 	*/
 
-	printf( "Finished boundary testing last 4 loops and else statement\n\n" );
+	printf( "Finished boundary testing last 4 loops\n\n" );
 
 	return numErrors;
 
